8e7c1ee9a3edb36af8b315e2829019af9a101af1
Saerom <non1233@naver.com>
2024-01-12 15:22:19 +0900

    feat : LinkedList 과제 1-3 사용자의 입력으로 숫자를 입력 받아서 (마이너스도 허용) 음수는 앞에 추가하고, 양수는 뒤에 추가하여 음수&양수를 반으로 나누는 연결리스트 구현 입력 받을 때마다 처음부터 끝까지 출력 진행

---
+++ 02. Linked List/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _02._Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> numLinks = new LinkedList<int>();
+            while(true)
+            {
+                Console.Write("숫자를 입력하시오(0입력시 종료) : ");
+                int input = int.Parse(Console.ReadLine());
+                if (input < 0)
+                    numLinks.AddFirst(input);
+                else if (input > 0)
+                    numLinks.AddLast(input);
+                else
+                    break;
+                Console.Write("리스트 목록 : ");
+                foreach (int link in numLinks)
+                    Console.Write(link+" ");
+                Console.WriteLine();
+            }
+        }
+
+        
+    }
+}
