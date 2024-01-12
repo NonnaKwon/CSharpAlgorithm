55f705dcb0409c29058595e2931b9eb271138cff
Saerom <non1233@naver.com>
2024-01-12 10:53:01 +0900

    feat : List 과제 1-5번
    사용자에게 숫자를 입력 받아서
    0부터 숫자까지 가지는 리스트 만들기
    0 요소를 제거
    리스트가 가지는 모든 요소들을 출력해주는 반복문 작성
    
    사용자의 입력을 받아서 없는 데이터면 추가, 있던 데이터면 삭제하는 보관함
    ex) 1, 6, 7, 8, 3 들고 있던 상황이면
    2 입력하면 없던 경우니까 1, 6, 7, 8, 3, 2
    7 입력하면 있던 경우니까 1, 6, 8, 3

--- 01.List/Program.cs
+++ 01.List/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("숫자를 입력하시오 : ");
+            int num = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>(num);
+            for (int i = 0; i <= num; i++)
+            {
+                list.Add(i);
+            }
+            list.Remove(0);
+
+            PrintList(list);
+
+            while (true)
+            {
+                Console.Write("숫자를 입력하시오(종료는 -1) : ");
+                int input = int.Parse(Console.ReadLine());
+                if (input == -1)
+                    break;
+
+                if (list.Contains(input))
+                    list.Remove(input);
+                else
+                    list.Add(input);
+            }
+            PrintList(list);
+
+        }
+
+        static public void PrintList(List<int> list)
+        {
+            Console.WriteLine("==========List 요소들==========");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + ", ");
+            }
+            Console.WriteLine();
         }
     }
 }
