f0b0ed0d483f147c6368e40ff739252ec6392897
Saerom <non1233@naver.com>
2024-01-15 10:32:13 +0900

    feat : 스택 스택 공부

---
+++ 03. Stack/Stack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure
+{
+    internal class Stack<T>
+    {
+        private List<T> container;
+
+        public Stack() 
+        { 
+            container = new List<T>();
+        }
+
+        public int Count { get { return container.Count; } }
+
+        public void Push(T item)
+        {
+            container.Add(item);
+        }
+
+        public T Pop()
+        {
+            T item = container[container.Count - 1];
+            container.RemoveAt(container.Count - 1);
+            return item;
+        }
+    }
+}
