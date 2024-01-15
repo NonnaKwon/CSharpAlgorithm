0040fb7e0553386fa15ede62b5080efa7ac82263
Saerom <non1233@naver.com>
2024-01-15 14:25:44 +0900

    feat : 큐 작업 프로세스 작업 프로세스 만들기

---
+++ 03-3. 작업 프로세스/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03_3._작업_프로세스
+{
+    internal class Program
+    {
+        public static int[] ProcessJob(int[] jobList)
+        {
+            int[] result = new int[jobList.Length];
+            Queue<int> queue = new Queue<int>(jobList);
+
+            int workTime = 8;
+            int day = 1;
+            int index = 0;
+            while (queue.Count > 0)
+            {
+                int todayJob = queue.Peek();
+                while (todayJob > workTime)
+                {
+                    todayJob -= workTime;
+                    day++;
+                    workTime = 8;
+                }
+
+                workTime -= todayJob;
+                queue.Dequeue();
+                result[index++] = day;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int[] input = { 4, 4, 12, 10, 2, 10 };
+            int[] result = ProcessJob(input);
+            foreach (int num in result)
+            {
+                Console.Write(num + ",");
+            }
+        }
+    }
+}
