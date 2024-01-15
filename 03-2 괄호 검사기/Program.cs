43d392fb6c67c7648061b842136c44df0d666b6b
Saerom <non1233@naver.com>
2024-01-15 13:51:47 +0900

    feat : 괄호 검사기 구현하기 스택과 큐를 이용하여 괄호 검사기를 구현하자.

---
+++ 03-2 괄호 검사기/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _03_2_괄호_검사기
+{
+    internal class Program
+    {
+        static bool IsOk(string text)
+        {
+            Stack<char> stack = new Stack<char> ();
+            foreach(char c in text)
+            {
+                if (c == ']')
+                {
+                    if (stack.Peek() != '[')
+                        return false;
+                    stack.Pop();
+                }
+                else if (c == '}')
+                {
+                    if (stack.Peek() != '{')
+                        return false;
+                    stack.Pop();
+                }
+                else if (c == ')')
+                {
+                    if (stack.Peek() != '(')
+                        return false;
+                    stack.Pop();
+                }
+                else
+                    stack.Push(c);
+            }
+
+            return stack.Count == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsOk("()"));
+            Console.WriteLine(IsOk("(()"));
+            Console.WriteLine(IsOk("[)"));
+            Console.WriteLine(IsOk("[[(){}]]"));
+        }
+    }
+}
