66a5d8a400e8b08c7a41fd2c8199e2dc19e26db4
Saerom <non1233@naver.com>
2024-01-17 17:16:17 +0900

    feat : 백준 13904 과제 문제 찬규오빠와의 합작

---
+++ 06-1. 웅찬이의 과제/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Xml.Linq;
+
+namespace _06_1._웅찬이의_과제
+{
+    internal class Program
+    {
+        class MyData
+        {
+            public int d;
+            public int w;
+            public MyData(int d,int w)
+            {
+                this.d = d;
+                this.w = w;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            PriorityQueue<MyData, int> pq = new PriorityQueue<MyData, int>();
+            
+
+            int N = int.Parse(Console.ReadLine());
+            int dayMax = 0;
+            while(N-- > 0)
+            {
+                string s = Console.ReadLine();
+                string[] ss = s.Split(' ');
+                MyData myData = new MyData(int.Parse(ss[0]), int.Parse(ss[1]));
+                pq.Enqueue(myData, -1*myData.w);
+                if(myData.d > dayMax)
+                {
+                    dayMax = myData.d;
+                }
+            }
+
+            List<int> sc = new List<int>();
+            for (int i = 0; i <= dayMax; i++)
+                sc.Add(0);
+
+            while(pq.Count > 0)
+            {
+                MyData md = pq.Dequeue();
+                int day = md.d;
+                while (0 < day && sc[day] != 0)
+                    day--;
+                if(0 < day && sc[day] == 0)
+                    sc[day] = md.w;
+            }
+
+            int result = 0;
+            for (int i = 0; i < sc.Count; i++)
+                result += sc[i];
+            Console.WriteLine(result);
+        }
+    }
+
+
+}
