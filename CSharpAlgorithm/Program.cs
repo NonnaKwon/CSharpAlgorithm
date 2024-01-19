7afbae9f95aed0c762a2341dd7d1deefa1ab82b1
Saerom <non1233@naver.com>
2024-01-19 15:52:17 +0900

    feat : 백준 1932 정수 삼각형

--- CSharpAlgorithm/Program.cs
+++ CSharpAlgorithm/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<List<int>> lists = new List<List<int>>();
+
+            //입력받기
+            int N = int.Parse(Console.ReadLine());
+            for(int i=0;i<N;i++)
+            {
+                List<int> list = new List<int>();
+                string input = Console.ReadLine();
+                string[] inputs = input.Split(' ');
+                for(int j=0;j<inputs.Length; j++)
+                    list.Add(int.Parse(inputs[j]));
+                lists.Add(list);
+            }
+
+
+            //풀이
+            for(int i=1;i<N;i++)
+            {
+                for(int j = 0; j < lists[i].Count;j++)
+                {
+                    if (lists[i - 1].Count == j)
+                        lists[i][j] += lists[i - 1][j - 1];
+                    else if (j == 0)
+                        lists[i][j] += lists[i - 1][j];
+                    else
+                        lists[i][j] += lists[i - 1][j] > lists[i - 1][j - 1] ? lists[i - 1][j] : lists[i - 1][j - 1];
+                }
+            }
+
+            int max = lists[0][0];
+            for(int i = 0; i < lists[N-1].Count; i++)
+            {
+                if(max < lists[N-1][i])
+                    max = lists[N - 1][i];
+            }
+
+            Console.WriteLine(max);
+
         }
     }
 }
