5b75a32e798dfcf7bcadfbc4e5e9f870fe0089a9
Saerom <non1233@naver.com>
2024-01-23 14:26:51 +0900

    feat : Grape 실습 1

---
+++ 10. Graph/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _10._Graph
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[][] solution1 = new int[8][];
+            solution1[0] = new int[] { 2, 4 };
+            solution1[1] = new int[] { 2, 5 };
+            solution1[2] = new int[] { 0, 4, 5, 6 };
+            solution1[3] = new int[] { 7 };
+            solution1[4] = new int[] { 0, 7 };
+            solution1[5] = new int[] { 1, 2 };
+            solution1[6] = new int[] { 2 };
+            solution1[7] = new int[] { 3, 4 };
+            Print(solution1 );
+        }
+
+        static void Print(int[][] solution)
+        {
+            for(int i=0;i<solution.Length; i++)
+            {
+                Console.Write($"{i}번째 : ");
+                for (int j = 0; j < solution[i].Length; j++)
+                {
+                    Console.Write(solution[i][j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 10. 그래프 인접행렬 표현/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _10._그래프_인접행렬_표현
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] solution1 = new bool[8, 8]
+            {   //0 1 2 3 4 5 6 7
+                { false,false,true,false,true,false,false,false},
+                { false,false,true,false,false,true,false,false},
+                { true,true,false,false,true,true,false,false},
+                { false,false,false,false,false,false,false,true},
+                { true,false,false,false,false,false,false,true},
+                { false,true,true,false,false,false,false,false},
+                { false,false,true,false,false,false,false,false},
+                { false,false,false,true,true,false,false,false},
+            };
+
+            bool[,] solution2 = new bool[8, 8]
+            {   //0 1 2 3 4 5 6 7
+                { false,false,false,false,false,false,false,false},
+                { false,false,false,false,false,false,false,false},
+                { false,false,false,false,true,true,false,false},
+                { false,true,false,false,false,true,false,true},
+                { false,false,false,false,false,false,false,false},
+                { false,true,false,false,false,false,false,false},
+                { false,false,true,false,false,true,false,false},
+                { false,false,false,false,false,false,true,false},
+            };
+
+            const int INF = int.MinValue;
+            int[,] solution3 = new int[8, 8]
+            {
+                { INF,4,INF,INF,6,INF,INF,INF},
+                { 4,INF,5,4,INF,8,INF,INF},
+                { INF,5,INF,INF,9,INF,INF,INF},
+                { INF,4,INF,INF,INF,INF,INF,INF},
+                { 6,INF,9,INF,INF,INF,5,INF},
+                { INF,8,INF,INF,INF,INF,INF,1},
+                { INF,2,INF,INF,5,INF,INF,INF},
+                { INF,INF,INF,INF,INF,1,INF,INF},
+            };
+
+        }
+    }
+}
